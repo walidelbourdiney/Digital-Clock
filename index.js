7bde5443138a94ecc1d1cc93deb9e7b4b71692d6
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-25 16:05:57 +0200

    revising JS

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 "use strict";
 
-function updateClock() {
-  const clock = document.getElementById("clock");
-  let now = new Date().toLocaleTimeString("en-US");
-  clock.innerText = now;
-}
+// function updateClock() {
+//   const clock = document.getElementById("clock");
+//   let now = new Date().toLocaleTimeString("en-US");
+//   clock.innerText = now;
+// }
 
-setInterval(updateClock, 1000);
+// setInterval(updateClock, 1000);
+
+const clock = document.getElementById("clock");
+
+
+const names = [
+  "Liam",
+  "Sofia",
+  "Ethan",
+  "Isabella",
+  "Hiroshi",
+  "Amina",
+  "Mateo",
+  "Anika",
+  "Kofi",
+  "Freya"
+];
+
+let random = Math.floor(Math.random() * names.length);
+console.log(names[random])
