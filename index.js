d9e95de43e5e53810a08ad898f2ccd76d44aea3e
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-25 17:58:48 +0200

    generate password

--- index.js
+++ index.js
@@ -11,18 +11,53 @@
 const clock = document.getElementById("clock");
 
 
-const names = [
-  "Liam",
-  "Sofia",
-  "Ethan",
-  "Isabella",
-  "Hiroshi",
-  "Amina",
-  "Mateo",
-  "Anika",
-  "Kofi",
-  "Freya"
-];
+// const names = [
+//   "Liam",
+//   "Sofia",
+//   "Ethan",
+//   "Isabella",
+//   "Hiroshi",
+//   "Amina",
+//   "Mateo",
+//   "Anika",
+//   "Kofi",
+//   "Freya"
+// ];
 
-let random = Math.floor(Math.random() * names.length);
-console.log(names[random])
+// let random = Math.floor(Math.random() * names.length);
+// // console.log(names[random])
+
+// console.log(names.slice().reverse())
+
+// let numbers = [1,2,3,4,5,6,7,8,9]
+// console.log(numbers)
+// ----------------------------------------------------------------- //
+//generate Password:
+
+function generatePassword(max, lower, upper, numbers, symbols){
+
+const lowerCase = 'abcdefghijklmnopq'
+const upperCase = 'ABCDEFGHIJKLMNOPQ'
+const numberss = '1234567890'
+const symbolss = '!@#$%^&*()_+'
+
+let allowedCharacters = ''
+let passWord = ''
+
+allowedCharacters += lower? lowerCase: '';
+allowedCharacters += upper? upperCase: '';
+allowedCharacters += numbers? numberss: '';
+allowedCharacters += symbols? symbolss: '';
+
+for(let i=0;i<max;i++){
+    let random = Math.floor(Math.random() * allowedCharacters.length) + 1
+    passWord += allowedCharacters[random];
+}
+
+return passWord;
+
+
+
+}
+let x= generatePassword(12, true, true, true, true)
+console.log(x);
